93ead540930e3468af5b31cb94e7f0de292c8882
Simphiwe Mabaso <Simphiwe.Mabaso@hollywoodbets.net>
2022-04-05 17:06:19 +0200

    Learning to organise project layers in C#

---
+++ source/repos/Tutorial/BLL/Employees.cs
@@ -0,0 +1,33 @@
+﻿using DAL;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Employees
+    {
+        private readonly IDbConnection _DataBaseConnection;
+        
+        public Employees(IDbConnection DataBaseConnection)
+        {
+            _DataBaseConnection = DataBaseConnection;
+        }
+
+        public List<BO.Employee> GetEmpoyees()
+        {
+            List<BO.Employee> employee = null;
+            using (var db = new SqlConnection(_DataBaseConnection.ConnectionString["TestDB"].ConnectionString))
+            {
+                employee = db.Query<BO.Employee>("dbo.GetEmployees", commandType: CommandType.StoredProcedure).ToList();
+            }
+
+            return employee;
+        }
+    }
+}
---
+++ source/repos/Tutorial/DAL/DataAccess.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataAccess
+    {
+        //public static IDbConnection DataBaseConnection { get { return new SqlConnection(ConfigurationManager.ConnectionStrings["TestDB"].ConnectionString); } }
+
+        //private static IDbConnection DataBaseConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["TestDB"].ConnectionString);
+        private readonly IDbConnection _DataBaseConnection;
+
+        public DataAccess(IDbConnection DataBaseConnection)
+        {
+            _DataBaseConnection = DataBaseConnection;
+        }
+
+        public string test()
+        {
+           var test = _DataBaseConnection.ConnectionString["TestDB"].ConnectionString;
+            return test;
+        }
+    }
+}
